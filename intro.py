36d9b7cafcf786f9e002f71c03fc50425d08aa0b
Ludvig <ludvig.alm@student.chasacademy.se>
2026-08-27 10:56:07 +0200

    Python uppgift klar

--- intro.py
+++ intro.py
@@ -1,1 +1,8 @@
-Print("Hej Läraren!")
+
+print("Hej!") # Välkomnar användaren
+
+name = input("Vad heter du? ") # Frågar användaren vad hen heter
+if name == "Sofia": # Sofia är min lärare
+    print("Hej läraren!") # Om användaren heter Sofia, hälsar vi på läraren
+else:
+    print("Hej " + name + "!") # Om användaren inte heter Sofia, hälsar vi på användaren som vanligt
