4b6c17e9ccbd85237002dc6de8832d3aff9edc27
Ludvig <ludvig.alm@student.chasacademy.se>
2026-08-28 13:47:48 +0200

    Stavade fel först på sophia

--- intro.py
+++ intro.py
@@ -2,7 +2,7 @@
 print("Hej!") # Välkomnar användaren
 
 name = input("Vad heter du? ") # Frågar användaren vad hen heter
-if name == "Sofia": # Sofia är min lärare
-    print("Hej läraren!") # Om användaren heter Sofia, hälsar vi på läraren
+if name == "Sophia": # Sophia är min lärare
+    print("Hej läraren!") # Om användaren heter Sophia, hälsar vi på läraren
 else:
-    print("Hej " + name + "!") # Om användaren inte heter Sofia, hälsar vi på användaren som vanligt
+    print("Hej " + name + "!") # Om användaren inte heter Sophia, hälsar vi på användaren som vanligt
